c5b7e0daa06b648076f9c8d0ed071450cb689681
YASIR AHAMAD <130695697+yasirahma@users.noreply.github.com>
2026-02-28 22:33:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_eks_node_group" "project" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+    source_security_group_ids = [aws_security_group.hdr_node_sg.id]
   }
 }
 
